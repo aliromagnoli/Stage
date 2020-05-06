75b6b5c922cc62c61eeb928f487dbce9417f6541
aliromagnoli <64159680+aliromagnoli@users.noreply.github.com>
2020-05-06 17:34:58 +0200

    Add files via upload

--- netflix 1999-2000.R
+++ netflix 1999-2000.R
@@ -6,7 +6,7 @@ setwd("C:/Users/alice/Desktop/stage/netflix 1999-2000")
 
 ##pacchetti e librerie utilizzati
 
-#library(reshape2) #per acast()
+library(reshape2) #per acast()
 library(randomForest) #per randomForest()
 library(rpart) #per rpart()
 
@@ -17,10 +17,8 @@ ls() #selected.data
 df <- selected.data
 rm(selected.data)
 df$id <- 1:nrow(df)
-df$customer <- as.numeric(df$customer)
-df$movie <- as.numeric(df$movie)
+df$customer <- as.factor(df$customer)
 colnames(df)[5] <- "rate"
-df$rate <- factor(df$rate)
 options(max.print=10000000)
 #####
 
@@ -53,6 +51,9 @@ if(offset <= dim(df)[1]){ #ci sono dati disponibili, continuo
     #L: insieme delle valutazioni effettuate sui film di I nel periodo h
     L <- df[(df$movie %in% I$movie | df$movie == record$movie) & df$day >= day1 & df$day < day2, ] 
     
+    #aggiungo record ad L 
+    L <- rbind(L, df[df$id == record$id,])
+    
     M <- data.frame()
     v <- 4
     
@@ -64,26 +65,33 @@ if(offset <= dim(df)[1]){ #ci sono dati disponibili, continuo
     }
     
     #D: matrice che ha per colonne i film di I e per righe gli utenti che hanno valutato almeno v film di I
-    #D <- acast(M, customer ~ movie , value.var='rate', dimnames=list(utenti=unique(M$customer), film=(M$movie)))
+    D <- acast(M, customer ~ movie , value.var='rate', dimnames=list(utenti=unique(M$customer), film=unique(M$movie)))
+    #conversione di D in un dataframe
+    D <- as.data.frame(D)
+    #conversione da int a factor degli elementi di D
+    D[sapply(D, is.integer)] <- lapply(D[sapply(D, is.integer)], as.factor)
 
 #7
-    M <- M[,3:5] #mantengo solo le colonne relative a customer, movie e rate
-    forest1 <- randomForest(rate ~ ., data=M) 
+    #riscrivo i nomi delle colonne in un formato accettabile
+    names(D) <- make.names(names(D)) #X + numero identificativo del film
+    record$movie #10013
+    #chiamo m il nome della colonna relativa al film m 
+    names(D)[names(D) == paste("X", record$movie, sep="")] <- 'm'
+    
+    set.seed(1)
+    forest <- randomForest(m ~ ., data=D, na.action = na.omit)
+    
     #forest2 <- randomForest(rate ~ ., data=M, importance=TRUE, proximity=TRUE)
     #OOB estimate of error rate: ~64% per entrambi
   
 #8
-    #considero u, m e rate=NA
-    a <- df[df$id == record$id, 3:5]
-    predict(forest1, a, type="prob") 
-    rate.predetto <- predict(forest1, a, type="response")
+    #trovo la riga corrispondente a u
+    index <- which(is.na(D$m)) #85
+    #predico il rate di u per m
+    predict(forest, D[index,], type="prob") 
+    rate.predetto <- predict(forest, D[index,], type="response")
     rate.predetto #rate predetto: 3
     record$rate #rate effettivo: 2
-    
-    #considero tutte le valutazioni effettuate dall'utente u + il rate NA per il movie m
-    b <- rbind(I, df[df$id==record$id,])
-    predict(forest1, b, type="response")
-    tail(predict(forest1, a, type="response"), 1) #rate predetto: 3
     
 #9 
     #differenza rate predetto e rate effettivo
@@ -91,10 +99,13 @@ if(offset <= dim(df)[1]){ #ci sono dati disponibili, continuo
     diff #differiscono di 1
     
 #10: tentativo con decision tree
-    decisionTree <- rpart(rate ~ ., data=M, method="class")
+    decisionTree <- rpart(m ~ ., data=D, method="class")
     print(decisionTree)
-    predict(decisionTree, a, type="class") #rate predetto: 3
-     
+    predict(decisionTree, D[index,], type="class") #rate predetto: 3
+    
+    #rinomino la colonna m con l'identificativo del film corrispondente
+    names(D)[names(D) == 'm'] <- paste("X", as.numeric(as.character(record$movie)), sep="")
+    
     #rendo nuovamente visibile il rate di record all'interno di df
     df[df$id == record$id, ]$rate <- record$rate
          
